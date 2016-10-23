21e543ce4fe860c7553ff05546f015b04436c9fc
Fabian Vilers <fabian.vilers@dev-one.com>
2016-10-23 13:12:00 +0200

    Added static files

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
+const path = require("path");
+const root = path.join(__dirname, "public");
 const app = express();
 
 // middlewares
 app.use(compression());
 app.use(helmet());
+app.use(express.static(root));
 app.all("*", (req, res) => {
-  res.sendStatus(404);
+  res.sendFile("index.html", { root: root });
 });
 
 module.exports = app;
