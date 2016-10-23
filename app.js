fc0c2fb1a3ed830a24c0fd09ecab9a0f820c1b16
Fabian Vilers <fabian.vilers@dev-one.com>
2016-10-23 13:09:13 +0200

    Added helmet

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const compression = require("compression");
+const helmet = require("helmet");
 const app = express();
 
 // middlewares
 app.use(compression());
+app.use(helmet());
 app.all("*", (req, res) => {
   res.sendStatus(404);
 });
