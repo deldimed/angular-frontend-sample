ffa670676452668ac02314378af8b0d704b442d1
Fabian Vilers <fabian.vilers@dev-one.com>
2016-10-23 13:08:42 +0200

    Added compression

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const express = require("express");
+const compression = require("compression");
 const app = express();
 
 // middlewares
+app.use(compression());
 app.all("*", (req, res) => {
   res.sendStatus(404);
 });
